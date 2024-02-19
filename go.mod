f485d139171994cda1ce0ef742f95cd2e22fe805
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2024-02-19 17:48:47 +1100

    Init command

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sparkymat/oxgen
 
 go 1.22.0
+
+require github.com/alecthomas/kong v0.8.1 // indirect
--- oxgen.go
+++ oxgen.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	app := OxgenApp{}
+	ctx := kong.Parse(
+		&app,
+		kong.Name("oxgen"),
+		kong.Description("A web-app generator"),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Compact: true,
+			Summary: true,
+		}),
+	)
+
+	err := ctx.Run(&kong.Context{})
+	ctx.FatalIfErrorf(err)
+}
+
+type OxgenApp struct {
+	Init InitCommand `cmd:"" help:"Initialize a new project"`
+}
+
+type InitCommand struct {
+	Force bool `help:"Forcibly initialize even if the folder is not empty."`
+}
+
+func (i *InitCommand) Run(ctx *kong.Context) error {
+	fmt.Println("I won't do that")
+	return nil
 }

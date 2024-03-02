040d208141f4afbe250579e3340d2945ca03ef4d
Ajith Hussain <ajith.hussain@fastmail.com>
2024-03-03 07:32:45 +1100

    Added types for resources

--- internal/generator/types.go
+++ internal/generator/types.go
@@ -1,9 +1,36 @@
 package generator
 
 type Config struct {
-	TemplatesFolder string   `json:"templatesFolder"`
-	ProjectID       string   `json:"projectId"`
-	RepoURL         string   `json:"repoUrl"`
-	PreCommands     []string `json:"preCommands"`
-	PostCommands    []string `json:"postCommands"`
+	TemplatesFolder string     `json:"templatesFolder"`
+	ProjectID       string     `json:"projectId"`
+	RepoURL         string     `json:"repoUrl"`
+	Resources       []Resource `json:"resources"`
+	PreCommands     []string   `json:"preCommands"`
+	PostCommands    []string   `json:"postCommands"`
 }
+
+type Resource struct {
+	Name        string  `json:"name"`
+	ServiceName string  `json:"serviceName"`
+	Fields      []Field `json:"fields"`
+}
+
+type Field struct {
+	Name     string    `json:"name"`
+	Type     FieldType `json:"type"`
+	Nillable bool      `json:"nillable"`
+	Unique   bool      `json:"unique"`
+	Index    bool      `json:"index"`
+	Default  string    `json:"default"`
+}
+
+type FieldType string
+
+const (
+	FieldString   FieldType = "string"
+	FieldInt      FieldType = "int"
+	FieldFloat    FieldType = "float"
+	FieldBool     FieldType = "bool"
+	FieldDate     FieldType = "date"
+	FieldDateTime FieldType = "datetime"
+)

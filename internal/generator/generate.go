1145865378ad5b3f61dd99e8dc23d5617ed97d83
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2024-04-12 09:10:06 +1000

    Added name validation

--- internal/generator/generate.go
+++ internal/generator/generate.go
@@ -2,8 +2,30 @@ package generator
 
 import (
 	"context"
+	"errors"
+
+	"github.com/gertd/go-pluralize"
+	"github.com/iancoleman/strcase"
 )
 
+var ErrInvalidResourceName = errors.New("invalid resource name")
+
 func (s *Service) Generate(ctx context.Context, name string) error {
+	if err := ensureValidResourceName(name); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ensureValidResourceName(name string) error {
+	if name == "" {
+		return ErrInvalidResourceName
+	}
+
+	if !pluralize.NewClient().IsSingular(name) || strcase.ToCamel(name) != name {
+		return ErrInvalidResourceName
+	}
+
 	return nil
 }

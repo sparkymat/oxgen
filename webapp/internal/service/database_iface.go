b54936a312512468f70f3dddf2dc6ca29b47caa4
Ajith Hussain <ajith.hussain@fastmail.com>
2024-04-16 07:26:46 +1000

    Removed generated stuff

--- webapp/internal/service/database_iface.go
+++ webapp/internal/service/database_iface.go
@@ -1,18 +1,3 @@
 package service
 
-type DatabaseProvider interface{
-  
-  CountSearchedUsers(ctx context.Context, query string) ([]int64, error) 
-  
-  CreateUser(ctx context.Context, params dbx.CreateUserParams) (dbx.User, error)
-  DeleteUser(ctx context.Context, id uuid.UUID) error 
-  FetchUserByID(ctx context.Context, id uuid.UUID) (dbx.User, error) 
-  FetchUsersByIDs(ctx context.Context, ids []uuid.UUID) ([]dbx.User, error) 
-  
-  SearchUsers(ctx context.Context, arg dbx.SearchUsersParams) ([]dbx.User, error) 
-  
-  UpdateUserName(ctx context.Context, arg dbx.UpdateUserNameParams) (dbx.User, error)
-  UpdateUserAge(ctx context.Context, arg dbx.UpdateUserAgeParams) (dbx.User, error)
-  UpdateUserDob(ctx context.Context, arg dbx.UpdateUserDobParams) (dbx.User, error)
-  UpdateUserPhotoPath(ctx context.Context, arg dbx.UpdateUserPhotoPathParams) (dbx.User, error)
-}
+type DatabaseProvider interface{}

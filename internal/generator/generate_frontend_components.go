7dc0fc2c42d64067b21c1e85288c7db5a7429c6d
Ajith Hussain <ajith.hussain@fastmail.com>
2024-05-04 23:52:38 +1000

    Generating show component

--- internal/generator/generate_frontend_components.go
+++ internal/generator/generate_frontend_components.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 )
 
-const frontendlistComponentTemplate = `
+const frontendListComponentTemplate = `
 import {
   Anchor,
   Button,
@@ -176,6 +176,135 @@ export const {{ .Resource.CamelcasePlural }}Page = () => {
 };
 `
 
+const frontendShowComponentTemplate = `
+import React, { useCallback, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  ActionIcon,
+  Box,
+  Button,
+  Container,
+  Flex,
+  Image,
+  LoadingOverlay,
+  Menu,
+  Paper,
+  Space,
+  Text,
+  Title,
+  rem,
+} from '@mantine/core';
+import { IconSettings, IconTrash } from '@tabler/icons-react';
+
+import { EditableImage } from '../EditableImage';
+import { EditableTextField } from '../EditableTextField';
+import { {{ .Resource.CamelcaseSingular }} } from '../../models/{{ .Resource.CamelcaseSingular }}';
+import {
+  useShowQuery,
+  useUploadIconMutation,
+  useUpdateNameMutation,
+  useDestroyMutation,
+} from '../../slices/{{ .Resource.CamelcaseSingular }}';
+import { modals } from '@mantine/modals';
+
+export const {{ .Resource.CamelcaseSingular }}Page = () => {
+  const { id } = useParams();
+
+  const { data: itemData, isLoading } = useShowQuery(id || '');
+
+  const [uploadIcon] = useUploadIconMutation();
+  const [updateName] = useUpdateNameMutation();
+  const [destroyItem] = useDestroyMutation();
+
+  const item = useMemo(() => {
+    if (itemData) {
+      return new {{ .Resource.CamelcaseSingular }}(itemData);
+    }
+
+    return null;
+  }, [itemData]);
+
+  const imageUpdated = useCallback(
+    (file: File) => {
+      const formData = new FormData();
+      formData.append('icon_file', file);
+      uploadIcon({ id: id || '', formData });
+    },
+    [id, uploadIcon],
+  );
+
+  const nameUpdated = useCallback((name: string) => {
+    updateName({ id: id || '', name });
+  }, []);
+
+  const deleteClicked = useCallback(() => {
+    modals.openConfirmModal({
+      title: 'Are you sure you want to delete?',
+      centered: true,
+      labels: { confirm: 'Yes', cancel: 'No' },
+      confirmProps: { color: 'red' },
+      onConfirm: () => {
+        destroyItem(id || '').then(() => {
+          window.location.href = '/#/collections';
+        });
+      },
+    });
+  }, [destroyItem, id]);
+
+  return (
+    <Container>
+      <Flex direction="column">
+        <Paper p="sm">
+          <Flex wrap="wrap" justify="center">
+            <Box style={{ "{{ position: 'relative' }}" }} w={280} h={280}>
+              <EditableImage
+                style={{ "{{}}" }}
+                src={item?.icon || ''}
+                w={280}
+                h={280}
+                onImageUpdated={imageUpdated}
+              />
+            </Box>
+            <Flex direction="column" ml="sm" gap="md" style={{ "{{ flex: 1 }}" }}>
+              <Flex justify="space-between">
+                <EditableTextField
+                  currentValue={item?.name || ''}
+                  onValueSubmitted={nameUpdated}
+                >
+                  <Title order={1}>{item?.name}</Title>
+                </EditableTextField>
+                <Menu shadow="md" width={200}>
+                  <Menu.Target>
+                    <ActionIcon>
+                      <IconSettings size="2rem" stroke={1.5} color="gray" />
+                    </ActionIcon>
+                  </Menu.Target>
+
+                  <Menu.Dropdown>
+                    <Menu.Item
+                      color="red"
+                      leftSection={
+                        <IconTrash
+                          style={{ "{{ width: rem(14), height: rem(14) }}" }}
+                        />
+                      }
+                      onClick={deleteClicked}
+                    >
+                      Delete
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Paper>
+      </Flex>
+      <LoadingOverlay visible={isLoading} />
+    </Container>
+  );
+};
+`
+
 func (s *Service) generateFrontendComponents(ctx context.Context, input Input) error {
 	folderPath := filepath.Join(input.WorkspaceFolder, "frontend", "src", "components", input.Resource.CamelcasePlural()+"Page")
 
@@ -192,7 +321,28 @@ func (s *Service) generateFrontendComponents(ctx context.Context, input Input) e
 		0,
 		"",
 		"frontendList",
-		frontendlistComponentTemplate,
+		frontendListComponentTemplate,
+		input,
+	); err != nil {
+		return fmt.Errorf("failed to generate frontend slice: %w", err)
+	}
+
+	folderPath = filepath.Join(input.WorkspaceFolder, "frontend", "src", "components", input.Resource.CamelcaseSingular()+"Page")
+
+	if err := s.ensureFolderExists(folderPath); err != nil {
+		return fmt.Errorf("failed to ensure frontend show component folder exists: %w", err)
+	}
+
+	filename = "index.tsx"
+
+	filePath = filepath.Join(folderPath, filename)
+	if err := s.appendTemplateToFile(
+		ctx,
+		filePath,
+		0,
+		"",
+		"frontendShow",
+		frontendShowComponentTemplate,
 		input,
 	); err != nil {
 		return fmt.Errorf("failed to generate frontend slice: %w", err)
